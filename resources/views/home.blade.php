93b7c6cf2d0627dc1c1c1f680230e27d1d55823e
Dario <dario.defrancisci92@gmail.com>
2021-10-26 16:32:01 +0200

    stampo Hello World

---
+++ resources/views/home.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+        
+    </head>
+    <body>
+        <div class="content">
+            <div class="title m-b-md">
+                Hello World
+            </div>
+
+            <div class="links">
+                <a href="/homepage">Homepage</a>
+                <a href="/about">About</a>
+                <a href="/contacts">Contacts</a>
+            </div>
+        </div>
+    </body>
+</html>
